d89c0c0c5ed693391190f19579ef09d9fe37b038
Romain Endelin <romain.endelin@gmail.com>
2014-06-05 18:14:16 +0200

    Implemented the semantic engine

--- capsule_corp/semantic_engine/cogitation.py
+++ capsule_corp/semantic_engine/cogitation.py
@@ -1,40 +1,58 @@
 import glob
 import subprocess
+from tempfile import mktemp
 from rdflib import Graph, Namespace, Literal
+from rdflib.plugins import sparql
 
 from capsule_corp.semantic_engine import motion_estimator
 from capsule_corp.semantic_engine.ontology import (graph, qol, home,
                                                    ONTOLOGY_DIR)
 
 
-DUMP_FILE = '%s/dump-ubi.n3' % ONTOLOGY_DIR
 EULER_BIN = 'eye'
-EULER_KERNEL = ''
+
+event_query = sparql.prepareQuery(
+    """SELECT DISTINCT ?state ?sensor
+    WHERE {
+        ?sensorClass rdfs:subClassOf* qol:Sensor .
+        ?sensor rdf:type ?sensorClass .
+        ?sensor rdfs:label ?sensorLabel .
+        ?sensorClass qol:hasPossibleState ?state .
+        ?state rdfs:label ?stateLabel .
+        } LIMIT 1""",
+    initNs={'qol': qol})
 
 
 def save_to_ontology(event):
     """Saving an event into the graph"""
     graph.set((home['clock'],
-               qol['hasCurrentState'],
-               Literal(event['signal'])))
-    subject = home[event['sensor'].lower()]
-    graph.set((subject, qol['lastUpdate'], Literal(event['date'])))
+               qol['hasValue'],
+               Literal(event['date'])))
 
     if 'signal' in event:
         # if we deal with a sensor event
-        graph.set((subject, qol['hasCurrentState'], Literal(event['signal'])))
+        sparql_event = graph.query(
+            event_query,
+            initBindings={'sensorLabel': Literal(event['sensor']),
+                          'stateLabel': Literal(event['signal'])})
+
+        for row in sparql_event:
+            graph.set((row.sensor, qol['lastUpdate'], Literal(event['date'])))
+            graph.set((row.sensor, qol['hasLastUpdate'], Literal(True)))
+            graph.set((row.sensor, qol['hasCurrentState'], row.state))
 
 
 def think():
     """Runs the euler reasoning"""
     # Serialize the triple store in DUMP_FILE
-    graph.serialize(DUMP_FILE, format='n3')
+    dump_file = mktemp('.n3')
+    graph.serialize(dump_file, format='n3')
 
     # List required ontologies
     rule_files = glob.glob('%s/infer*.n3' % ONTOLOGY_DIR)
     query_files = glob.glob('%s/query*.n3' % ONTOLOGY_DIR)
 
-    euler_cmd = ([EULER_BIN, DUMP_FILE] +
+    euler_cmd = ([EULER_BIN, dump_file] +
                  rule_files +
                  ['--think', '--query'] +
                  query_files +
@@ -44,24 +62,24 @@ def think():
     process = subprocess.Popen(euler_cmd,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
-    return process.communicate
+    return process.communicate()
 
 
-def save():
+def save(eye_dump):
     """Place the euler output into the ontology"""
     dump_graph = Graph()
-    dump_graph.parse(DUMP_FILE, format='n3')
+    dump_graph.parse(data=eye_dump, format='n3')
     ts = Namespace("file://%s/infer-triplestore#" % ONTOLOGY_DIR)
 
-    # TODO: Check that formulae work
     for action, formula in dump_graph.predicate_objects(ts['n3store']):
-        for s, p, o in formula:
-            if action == ts['update']:
-                graph.set((s, p, o))
-            elif action == ts['add']:
-                graph.add((s, p, o))
-            elif action == ts['remove']:
-                graph.remove((s, p, o))
+        if action in (ts['update'], ts['add'], ts['remove']):
+            for s, p, o in formula:
+                if action == ts['update']:
+                    graph.set((s, p, o))
+                elif action == ts['add']:
+                    graph.add((s, p, o))
+                elif action == ts['remove']:
+                    graph.remove((s, p, o))
 
 
 def read_activity():
@@ -79,7 +97,7 @@ def infer_from_ontology(event):
     """For a new event, infer the activity, with ontological reasoning"""
     save_to_ontology(event)
     motion_estimator.estimate_motion()
-    think()
-    save()
+    out, err = think()
+    save(out)
     return read_activity()
     # TODO Close the graph at some time
--- capsule_corp/semantic_engine/motion_estimator.py
+++ capsule_corp/semantic_engine/motion_estimator.py
@@ -1,10 +1,11 @@
 from rdflib import Literal
 from rdflib.namespace import RDF
+import datetime
 
 from capsule_corp.semantic_engine.ontology import graph, qol, home
 
 
-TIME_WINDOW = 1 * 60 * 1000
+TIME_WINDOW = datetime.timedelta(minutes=1)
 
 
 class Room(object):
@@ -33,11 +34,14 @@ room_query = graph.query(
     WHERE {
         ?room rdf:type ?class .
         ?class rdfs:subClassOf* qol:Room .
+        ?room qol:partOf ?house .
     }""",
+    initBindings={'house': house},
     initNs={'qol': qol})
 rooms = {row.room: Room(row.room)
          for row in room_query}
 
+# We don't care of door sensors here
 sensor_query = graph.query(
     "SELECT DISTINCT ?sensor ?room WHERE { ?sensor qol:deployedIn ?room . }",
     initNs={'qol': qol})
@@ -47,12 +51,13 @@ sensors = {row.sensor: Sensor(row.sensor, rooms[row.room])
 
 def estimate_motion():
     """Saves the motion level of the patient into the ontology"""
-    clock = graph.value(home['clock'], qol['hasValue'])
+    clock = graph.value(home['clock'], qol['hasValue']).toPython()
 
     # set the room motions in the window frame
     for room in rooms.values():
         room.motion = 0
         for sensor in room.sensors:
+            # We exclude the old states
             sensor.on_states = [state
                                 for state in sensor.on_states
                                 if clock - state <= TIME_WINDOW]
@@ -60,12 +65,16 @@ def estimate_motion():
 
     # integrate new updates to the room motions
     for uri, sensor in sensors.items():
-        last_update = graph.value(uri, qol['lastUpdate'])
+        try:
+            last_update = graph.value(uri, qol['lastUpdate']).toPython()
+        except AttributeError:
+            last_update = None
 
-        if sensor.on_states and last_update != sensor.on_states[-1]:
+        if not sensor.on_states or last_update != sensor.on_states[-1]:
             state = graph.value(uri, qol['hasCurrentState'])
 
-            if (state, qol['indicateLocation'], Literal(True)) in graph:
+            if (state is not None and
+                (state, qol['indicateLocation'], Literal(True)) in graph):
                 sensor.on_states.append(last_update)
                 sensor.room.motion = sensor.room.motion + 1
 
